e25a88e53642f37d3d3da30b4f8519b9f0fbfa5c
jan.jajtner <jan.jajtner@gmail.com>
2026-05-04 12:23:20 +0200

    upd

--- src/app/l/[slug]/page.tsx
+++ src/app/l/[slug]/page.tsx
@@ -13,7 +13,7 @@ import { TerritoryMap } from "@/components/territory-map";
 import { formatMeters } from "@/lib/geo";
 import { after } from "next/server";
 import { getLocationPageData } from "@/lib/game";
-import { triggerEconomyTick } from "@/lib/trigger-economy";
+import { runEconomyTick } from "@/lib/economy";
 import { czechNameForType, normalizeLocationType } from "@/lib/location-types";
 
 function formatPopulation(population: number) {
@@ -34,7 +34,7 @@ function formatDate(date: string) {
 
 export default async function LocationPage(props: PageProps<"/l/[slug]">) {
   const { slug } = await props.params;
-  after(() => triggerEconomyTick());
+  after(() => runEconomyTick());
   const data = await getLocationPageData(slug);
 
   if (!data) {
